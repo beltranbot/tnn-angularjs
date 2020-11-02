038df2b7916d83603e205949fab71bf9bc016b36
carlos.beltran <beltranbot@gmail.com>
2020-11-02 14:26:42 -0500

    #15 ng-src directive

--- app/app.js
+++ app/app.js
@@ -3,10 +3,10 @@ var myNinjaApp = angular.module("myNinjaApp", []);
 myNinjaApp.controller("NinjaController", ["$scope", function ($scope) {
     $scope.message = "hey y'all";
     $scope.ninjas = [
-        { name: "Yoshi", belt: "Green", rate: 50, available: true },
-        { name: "Crystal", belt: "Yellow", rate: 30, available: true },
-        { name: "Ryu", belt: "Orange", rate: 10, available: false },
-        { name: "Shaun", belt: "Black", rate: 1000, available: true }
+        { name: "Yoshi", belt: "Green", rate: 50, available: true, thumb: "content/img/yoshi.png" },
+        { name: "Crystal", belt: "Red", rate: 30, available: true, thumb: "content/img/crystal.png" },
+        { name: "Ryu", belt: "Orange", rate: 10, available: false, thumb: "content/img/ryu.png" },
+        { name: "Shaun", belt: "Black", rate: 1000, available: true, thumb: "content/img/shaun.png" }
     ];
 
     $scope.removeNinja = function (ninja) {
