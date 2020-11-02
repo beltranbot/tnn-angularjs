941a07f26f77899efe010301496860da922e5dba
carlos.beltran <beltranbot@gmail.com>
2020-11-02 13:51:01 -0500

    #10 filters

--- app/app.js
+++ app/app.js
@@ -3,9 +3,9 @@ var myNinjaApp = angular.module("myNinjaApp", []);
 myNinjaApp.controller("NinjaController", ["$scope", function ($scope) {
     $scope.message = "hey y'all";
     $scope.ninjas = [
-        "Yoshi",
-        "crystal",
-        "ryu",
-        "shaun"
+        { name: "Yoshi", belt: "green", rate: 50 },
+        { name: "Crystal", belt: "Yellow", rate: 30 },
+        { name: "Ryu", belt: "Orange", rate: 10 },
+        { name: "Shaun", belt: "black", rate: 1000 }
     ];
 }]);
