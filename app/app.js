07df4e96fa4a948cb02e2740e4ad4d16688b167f
carlos.beltran <beltranbot@gmail.com>
2020-11-02 14:09:25 -0500

    #13 ng-click directive

--- app/app.js
+++ app/app.js
@@ -3,9 +3,15 @@ var myNinjaApp = angular.module("myNinjaApp", []);
 myNinjaApp.controller("NinjaController", ["$scope", function ($scope) {
     $scope.message = "hey y'all";
     $scope.ninjas = [
-        { name: "Yoshi", belt: "green", rate: 50, available: true },
+        { name: "Yoshi", belt: "Green", rate: 50, available: true },
         { name: "Crystal", belt: "Yellow", rate: 30, available: true },
         { name: "Ryu", belt: "Orange", rate: 10, available: false },
-        { name: "Shaun", belt: "black", rate: 1000, available: true }
+        { name: "Shaun", belt: "Black", rate: 1000, available: true }
     ];
+
+    $scope.removeNinja = function (ninja) {
+        var removedNinja = $scope.ninjas.indexOf(ninja);
+        $scope.ninjas.splice(removedNinja, 1);
+    };
+
 }]);
