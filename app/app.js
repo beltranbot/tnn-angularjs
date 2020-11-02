2282a812697b8ec4546e519f4833bea18906a426
carlos.beltran <beltranbot@gmail.com>
2020-11-02 13:59:39 -0500

    #12 ng-show directive

--- app/app.js
+++ app/app.js
@@ -3,9 +3,9 @@ var myNinjaApp = angular.module("myNinjaApp", []);
 myNinjaApp.controller("NinjaController", ["$scope", function ($scope) {
     $scope.message = "hey y'all";
     $scope.ninjas = [
-        { name: "Yoshi", belt: "green", rate: 50 },
-        { name: "Crystal", belt: "Yellow", rate: 30 },
-        { name: "Ryu", belt: "Orange", rate: 10 },
-        { name: "Shaun", belt: "black", rate: 1000 }
+        { name: "Yoshi", belt: "green", rate: 50, available: true },
+        { name: "Crystal", belt: "Yellow", rate: 30, available: true },
+        { name: "Ryu", belt: "Orange", rate: 10, available: false },
+        { name: "Shaun", belt: "black", rate: 1000, available: true }
     ];
 }]);
